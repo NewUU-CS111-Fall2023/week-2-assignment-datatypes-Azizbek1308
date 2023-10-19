8a77c247c4ac0e6a1b2947d34470d963d0dddce6
Azizbek Ergashov <20221500304@newuzbekistanuniversity.uz>
2023-10-20 02:10:25 +0500

    py changes

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -3,3 +3,69 @@
  * Date:
  * Name:
  */
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+class leap_year {
+public:
+    void leap_year1(int n) {
+        if (n % 4 == 0) {
+            std::cout << n << " is leap year" << std::endl;
+        }
+        else {
+            std::cout << n << " is not leap year" << std::endl;
+        }
+    }
+};
+
+class find_number {
+public:
+    void find_number1(int x) {
+        srand(time(0));
+        int n = rand() % 100 + 1;
+        while (x != n) {
+            if (x > n) {
+                std::cout << "too high" << std::endl;
+                std::cout << std::endl;
+                std::cout << "Please again guess a number!" << std::endl;
+                std::cin >> x;
+            }
+            else {
+                std::cout << "too low" << std::endl;
+                std::cout << std::endl;
+                std::cout << "Please again guess a number!" << std::endl;
+                std::cin >> x;
+            }
+        }
+        std::cout << "You found this " << x << " number! Congratulations!!! 🥳" << std::endl;
+    }
+};
+
+class long_arithmetics {
+public:
+    void long_arithmetics1(int A) {
+        srand(time(0));
+        long long x = rand() % (long long)(1e101 - 1e100) + (long long)1e100;
+        std::cout << x << " is random 100 digit number" << std::endl;
+        std::cout << (double)x / A << std::endl;
+    }
+};
+
+class string_matching {
+public:
+    void string_matching1(std::string x) {
+        if (x[x.size() - 1] != x.substr(0, x.size() - 1).find(x[x.size() - 1])) {
+            std::cout << x.substr(0, x.size() - 1).find(x[x.size() - 1]) << " is index of \"" << x[x.size() - 1] << "\" in \"" << x.substr(0, x.size() - 1) << "\" sentence!" << std::endl;
+        }
+        else {
+            std::cout << "no match" << std::endl;
+        }
+    }
+};
+
+int main() {
+    std::cout << "hi" << std::endl;
+    return 0;
+}
--- src/py/main.py
+++ src/py/main.py
@@ -1,2 +1,15 @@
 # * Author:
 # * Date:
+from Task import leap_year,find_number,long_arithmetics,string_matching
+# print("Task_1")
+# year = leap_year()
+# year.leap_year(int(input("Please enter a year for checking if it is leap year or not \n")))
+# print("Task_2")
+# number = find_number()
+# number.find_number(int(input("Please guess a number! \n")))
+# print("Task_3")
+# input_number = long_arithmetics()
+# input_number.long_arithmetics(float(input("Please enter a number! \n")))
+# print("Task_4")
+# string = string_matching()
+# string.string_matching(input("please enter a sentence and a word respectively! For example: hi my name is aziz is  \n").split(" "))
